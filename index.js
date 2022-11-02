084c1e93a6df1149b4e17ef6fd06835de1958bce
omerbenishu <omer.benishu@gmail.com>
2022-11-02 20:30:12 +0200

    show and hide functionallity

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ contactButton = document.querySelector("#contact button");
 contactButton.addEventListener("click", function contactClickhandler(el) {
   window.open("https://wa.me/972542181312","_self")
 });
+
+function showExtraStuff() {
+  document.getElementById('extrastuff').style.display = "block";
+}
+
+function exit() {
+  alert("You should consider your answer again in the next time")
+  document.getElementById('container').style.display = "none";
+
+}
